a80e6f358c929e9eb9f06b0efe13d4f1e2d11792
vinhngo1907 <vinhtrungngo1907@gmail.com>
2024-02-18 20:14:11 +0700

    mr airplane

--- DiemInterface.java
+++
@@ -1,7 +0,0 @@
-import java.rmi.*;
-
-public interface DiemInterface extends Remote{
-	public String layDiem() throws RemoteException;
-	public void ganDiem(int h, int t) throws RemoteException;
-	public void doiDiem(int dx, int dy) throws RemoteException;
-}
--- FlightInterface.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.RemoteException;
-import java.rmi.Remote;
-
-public interface FlightInterface extends Remote{
-	public String getFlightInformation() throws RemoteException;
-	public boolean buyTicket(int numberOfTicket) throws RemoteException;
-	public boolean refundTicket(int numberOfTicket) throws RemoteException;
-}
